44089615730d4230c631141b5fecd773b6683712
Set Hallstrom <set@hallstrom.ch>
2019-11-18 14:09:31 +0100

    insta feed update

--- _scripts/instalib.sh
+++ _scripts/instalib.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-cd /home/set/git/cuttlefish/_scripts/insta/
+cd /home/set/git/Cuttlefish-master/_scripts/
 
 npm run instalib -- liberate https://www.instagram.com/cuttlefish_se -o instagram.yml
 rm -f media/*.jpg
-rm -f /home/set/git/cuttlefish/images/insta/*
+rm -f /home/set/git/Cuttlefish-master/images/insta/*
 
 npm run instalib mirror instagram.yml 
-mogrify -resize 220 /home/set/git/cuttlefish/_scripts/media/*.jpg
+mogrify -resize 220 /home/set/git/Cuttlefish-master/_scripts/media/*.jpg
 
-mv /home/set/git/cuttlefish/_scripts/media/*.jpg /home/set/git/cuttlefish/images/insta/
-mv /home/set/git/cuttlefish/_scripts/instagram.yml /home/set/git/cuttlefish/_data/
+mv /home/set/git/Cuttlefish-master/_scripts/media/*.jpg /home/set/git/Cuttlefish-master/images/insta/
+mv /home/set/git/Cuttlefish-master/_scripts/instagram.yml /home/set/git/Cuttlefish-master/_data/
 
 chmod -x _scripts/instalib.sh
